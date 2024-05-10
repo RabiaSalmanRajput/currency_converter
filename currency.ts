da08ae7433cd69f13db3609327069b47c19fe31f
Rabia Rajput <160500537+RabiaSalmanRajput@users.noreply.github.com>
2024-05-10 12:41:15 -0700

    Add files via upload

---
+++ currency.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const currency: any = {
+    USD: 1, //Base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    IND: 74.57,
+    PKR: 280
+};
+
+let user_answer = await inquirer.prompt(
+    [
+        {
+            name: "from",
+            message: "Enter From Currency",
+            type: "list",
+            choices: ["USD", "EUR", "GBP", "IND", "PKR"]
+        },
+        {
+            name: "to",
+            message: "Enter To Currency",
+            type: "list",
+            choices: ["USD", "EUR", "GBP", "IND", "PKR"]
+        },
+        {
+            name: "amount",
+            message: "Enter Your Amount",
+            type: "number"
+        }
+    ]
+
+)
+
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount //USD base currency
+let convertedAmount = baseAmount = toAmount
+console.log(convertedAmount);
